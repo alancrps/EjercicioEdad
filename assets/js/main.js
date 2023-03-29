d93a25105556d588c15fd95dc6580b084572fb60
alancrps <alanfcorpus@gmail.com>
2023-03-29 14:32:04 -0300

    Se agrego nuevo input
    agregado del label e input nombre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 function esMayorDeEdad(){
     var edad = Number(document.getElementById("edad").value);
     var edadInput = document.getElementById("edad");
+
+    var nombre = document.getElementById("nombre").value;
+    var nombreInput = document.getElementById("nombre");
+
     if (edad <= 0){
         alert("La edad debe ser mayor a 0")
     }
     else if (edad >= 18){
-        alert(`Usted es mayor de edad (${edad})`);
+        alert(`Hola, ${nombre} sos mayor de edad (${edad})`);
     }
     else{
-        alert(`usted es menor de edad (${edad})`)
+        alert(`Hola, ${nombre} sos menor de edad (${edad})`)
     }
     edadInput.value="";
+    nombreInput.value = "";
 }
